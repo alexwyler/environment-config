5bdb8b55b29b2e25b1d6c9ed0bc7a4722c2a6cb4
Alex Wyler <alex.s.wyler@gmail.com>
2012-02-25 21:36:45 -0800

    Cleaned up emacs customization

---
+++ .emacs.d/awyler.el
@@ -0,0 +1,116 @@
+;
+; "She may not look like much, but she's got it where it counts."
+;
+
+(add-to-list 'load-path "~/.emacs.d/")
+
+; Auto-complete
+(require 'auto-complete-config)
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
+(ac-config-default)
+
+; JS2 Mode
+(autoload 'js2-mode "js2" nil t)
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+
+; Show line numbers
+(condition-case nil
+    (global-linum-mode t)
+  (error
+   (message "global-linum-mode not supported")))
+
+; No menus
+(setq menu-bar-mode nil)
+
+; Show column numbers in status buffer
+(setq column-number-mode t)
+
+; 2-space indentation
+(setq default-tab-width 2)
+(setq c-basic-offset 2)
+
+; No large file warning
+(setq large-file-warning-threshold nil)
+
+; Save place in buffer
+(require 'saveplace)
+(setq-default save-place t)
+
+; Ido
+(require 'ido)
+(setq ido-enable-flex-matching t)
+(setq ido-everywhere t)
+(condition-case nil
+    (ido-mode 1)
+  (error
+   (message "Failed to set start ido-mode")))
+
+; Mouse clicks
+(global-set-key
+ [mouse-1]
+ '(lambda (event)
+    (interactive "e")
+    (mouse-set-point event)))
+
+; Turn off bell
+(setq ring-bell-function 'ignore)
+
+; Rebind 'join-line -- M-^ is a stretch
+(global-set-key (kbd "C-x j") 'join-line)
+
+; Make cheating with arrow keys easier
+(global-set-key (kbd "C-<up>") 'backward-paragraph)
+(global-set-key (kbd "C-<down>") 'forward-paragraph)
+
+; As much as I love the splash screen...
+(setq inhibit-splash-screen t)
+
+; No scroll bars
+(setq scroll-bar-mode nil)
+
+; Auto revert-buffer
+(global-auto-revert-mode t)
+(setq auto-revert-verbose nil)
+
+; copy-paste over X
+(setq x-select-enable-clipboard t)
+(setq interprogram-paste-function 'x-cut-buffer-or-selection-value)
+
+; Color scheme
+(custom-set-faces
+ '(font-lock-builtin-face ((((class color) (background dark)) (:foreground "#C776D0"))))
+ '(font-lock-comment-face ((t (:foreground "#808080"))))
+ '(font-lock-constant-face ((((class color) (background dark)) (:bold t :foreground "#2FC3FE"))))
+ '(font-lock-doc-string-face ((t (:foreground "#dd8ddc"))))
+ '(font-lock-function-name-face ((t (:foreground "#F5F24E"))))
+ '(font-lock-keyword-face ((t (:bold t :foreground "#2FC3FE"))))
+ '(font-lock-preprocessor-face ((t (:italic nil :foreground "#A0A0A0"))))
+ '(font-lock-reference-face ((t (:foreground "#FAFA5A"))))
+ '(font-lock-string-face ((t (:foreground "#99ff61"))))
+ '(font-lock-type-face ((t (:foreground "#F5F24E"))))
+ '(font-lock-variable-name-face ((t (:foreground "#F57A7A"))))
+ '(font-lock-warning-face ((t (:bold t :foreground "#F57A7A"))))
+ '(compilation-info ((t (:foreground  "#2FC3FE"))))
+ '(minibuffer-prompt ((((background dark)) (:foreground "#2FC3FE")))))
+(set-foreground-color "#F1F1F1")
+(set-background-color "#1E1E1E")
+(set-face-foreground 'minibuffer-prompt "#F5F24E")
+
+; Font
+(set-face-attribute 'default nil :family "consolas" :height 130)
+
+; pretty colors in eshell
+(autoload 'ansi-color-for-comint-mode-on "ansi-color" nil t)
+(add-hook 'shell-mode-hook
+          'ansi-color-for-comint-mode-on)
+
+; Highlight matching parens
+(show-paren-mode t)
+
+; Hilight current line
+(load-library "hl-line.el")
+(global-hl-line-mode 1)
+(set-face-background 'hl-line "#101010")
+
+; Start emacs server
+(server-start)
